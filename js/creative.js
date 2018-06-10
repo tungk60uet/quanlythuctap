76be4b80470c420a0fd9f2f463b5863ca0c162e3
Tung Teen <tungk60uet@gmail.com>
2018-06-10 11:59:09 +0700

    add mailbox

--- js/creative.js
+++ js/creative.js
@@ -1,6 +1,15 @@
+jQuery('.dropdown-toggle').on('click', function (e) {
+  $(this).next().toggle();
+});
+jQuery('.dropdown-menu.keep-open').on('click', function (e) {
+  e.stopPropagation();
+});
+
 (function($) {
   "use strict"; // Start of use strict
-
+  $('.mailComposeBtn, .mailReplyBtn').click(function() {
+    $('#composeMail').modal('toggle')
+  });
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
